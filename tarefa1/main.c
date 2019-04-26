6753b55f383da105a32b572b78e8c905629da61e
Guilherme Camargo Valese <guilherme.valese@hotmail.com>
2019-04-26 15:27:32 -0300

    Update main.c

--- tarefa1/main.c
+++ tarefa1/main.c
@@ -6,11 +6,13 @@
  *
  *  Obs: Este programa é compilado somente no Windows
  */
-#include <stdio.h>
-#include <stdlib.h>
+#include <complex.h>
 #include <conio.h>
+#include <ctype.h>
 #include <locale.h>
 #include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define PI 3.141592654
 
@@ -23,102 +25,114 @@ int main()
     //Variáveis utilizadas para a opção 'c' (cálculo de reatâncias)
     float C, L, freq;
 
-    while(user_selection != 's')
+    while(user_selection != 'S')
     {
         system("cls");
         printf("     MENU PRINCIPAL\nDigite:\n<a> Associação de impedâncias em Série\n<b> Associação de impedâncias em Paralelo\n<c> Cálculo de reatâncias\n<d> Conversor de impedâncias P->R e R->P\n<e> Divisor de tensão\n<f> Divisor de corrente\n<s> Sair\n");
         fflush(stdin);
-        user_selection = getch();
+        user_selection = toupper(getch());
         switch (user_selection)
         {
-        case 'a':   // Submenu Associação de impedâncias em série
+        case 'A':   // Submenu Associação de impedâncias em série
             do
             {
                 system("cls");
                 printf("ASSOCIAÇÃO DE IMPEDÂNCIAS EM SÉRIE\nDigite:\n<a> Entre com o valor da impedância 1 (forma retangular)\n<b> Entre com o valor da impedância 2 (forma retangular)\n<c> Apresentar impedância equivalente\n<r> Retornar ao menu principal\n");
                 fflush(stdin);
-                user_sub_selection = getch();
+                user_sub_selection = toupper(getch());
                 switch (user_sub_selection)
                 {
-                case 'a': // Solicita o valor da impedancia 1
+                case 'A': // Solicita o valor da impedancia 1
                     printf("\nInforme o valor da impedância 1 (forma retangular Z=R+jX)\nDigite o valor de R e confirme com a tecla Enter: ");
                     while(scanf("%f", &R1) != 1) // Armazena a parte real da impedancia
                     {
-                        printf("\nInforme um valor válido!");
+                        printf("\nInforme um valor válido!\nR: ");
                         fflush(stdin);
                     }
                     printf("Digite o valor de X e confirme com a tecla Enter: ");
                     while(scanf("%f", &X1) != 1) // Armazena a parte imaginária da impedância
                     {
-                        printf("\nInforme um valor válido!");
+                        printf("\nInforme um valor válido!\nX: ");
                         fflush(stdin);
                     }
                     break;
 
-                case 'b': // Solicita o valor da impedancia 2
+                case 'B': // Solicita o valor da impedancia 2
                     printf("\nInforme o valor da impedância 2 (forma retangular Z=R+jX)\nDigite o valor de R e confirme com a tecla Enter: ");
                     while(scanf("%f", &R2) != 1)
                     {
-                        printf("\nInforme um valor válido!");
+                        printf("\nInforme um valor válido!\nR: ");
                         fflush(stdin);
                     }
                     printf("Digite o valor de X e confirme com a tecla Enter: ");
                     while(scanf("%f", &X2) != 1) // Armazena a parte imaginária da impedância
                     {
-                        printf("\nInforme um valor válido!");
+                        printf("\nInforme um valor válido!\nX: ");
                         fflush(stdin);
                     }
                     break;
 
-                case 'c': // Apresenta o valor da impedancia equivalente
+                case 'C': // Apresenta o valor da impedancia equivalente
                     printf("\n A impedância equivalente Zeq = %g + %gi Ohms\n", (R1+R2), (X1+X2));
                     system("pause");
                     break;
-
+                        
+                default:
+                    if(user_sub_selection != 'R')
+                    {
+                         printf("Opção inválida.");
+                         system("sleep 1");
+                    }
+                    break;
+                        
                 }
             }
-            while (user_sub_selection != 'r');
-
+            while (user_sub_selection != 'R');
             break;  // Fim do submenu de impedância série
 
-        case 'b':   // Início do submenu de impedância em paralelo
+        case 'B':   // Início do submenu de impedância em paralelo
             do
             {
                 fflush(stdin);
                 system("cls");
                 printf("ASSOCIAÇÃO DE IMPEDÂNCIAS EM PARALELO\nDigite:\n<a> Entre com o valor da impedância 1 (forma polar)\n<b> Entre com o valor da impedância 2 (forma polar)\n<c> Apresentar impedância equivalente\n<r> Retornar ao menu principal\n");
-
-                printf("\nAINDA EM IMPLEMENTAÇÃO . . .");
-
-                user_sub_selection = getch();
+                user_sub_selection = toupper(getch());
                 switch (user_sub_selection)
                 {
-                case 'a': // Solicita o valor da impedância 1
+                case 'A': // Solicita o valor da impedância 1
+                    
+                    break;
+
+                case 'B': // Solicita o valor da impedância 2
 
                     break;
 
-                case 'b': // Solicita o valor da impedância 2
+                case 'C': // Apresenta o resultado equivalente
 
                     break;
-
-                case 'c': // Apresenta o resultado equivalente
-
+                
+                default:
+                    if(user_sub_selection != 'R')
+                    {
+                        printf("Opção Inválida.");
+                        system("sleep 1");
+                    }
                     break;
                 }
             }
-            while(user_sub_selection != 'r');  // Se o usuário digitar 'r', volta ao menu principal
+            while(user_sub_selection != 'R');  // Se o usuário digitar 'r', volta ao menu principal
             break;  // Fim do submenu de impedância em paralelo
 
-        case 'c':   // Início do submenu de cálculo de reatâncias
+        case 'C':   // Início do submenu de cálculo de reatâncias
             do
             {
                 fflush(stdin);
                 system("cls");
                 printf("CÁLCULO DE REATÂNCIAS\nDigite:\n<a> Para calcular a reatância capacitiva\n<b> Para calcular a reatância indutiva\n<r> Retornar ao menu principal\n");
-                user_sub_selection = getch();
+                user_sub_selection = toupper(getch());
                 switch (user_sub_selection)
                 {
-                case 'a':   // Cálculo da reatância capacitiva
+                case 'A':   // Cálculo da reatância capacitiva
                     printf("Informe o valor da frequência em Hertz e confirme com a tecla Enter.\nF= ");
                     while(scanf("%f", &freq) != 1)
                     {
@@ -136,7 +150,7 @@ int main()
                     system("pause");
                     break;   // Fim do cálculo da reatância capacitiva
 
-                case 'b':   // Cálculo da reatência indutiva
+                case 'B':   // Cálculo da reatência indutiva
                     printf("Informe o valor da frequência (Hz) e confirme com a tecla Enter\nF= ");
                     while(scanf("%f", &freq) != 1)
                     {
@@ -153,24 +167,41 @@ int main()
                     printf("Reatância Indutiva\n\tXl=%.3fi Ohms \n", (2*PI*freq*L));
                     system("pause");
                     break; // Fim do cálculo da reatância indutiva
+                        
+                    default:
+                        if(user_sub_selection != 'R')
+                        {
+                            printf("Opção inválida");
+                            system("sleep 1");
+                        }
+                        break;
                 }
             }
-            while(user_sub_selection != 'r');  // Se o usuário digitar 'r' volta ao menu principal
+            while(user_sub_selection != 'R');  // Se o usuário digitar 'r' volta ao menu principal
             break;  // Fim do submenu de cálculo de reatâncias
 
-        case 'e':   // Divisor de Tensão
+            case 'D': // Conversão de Impedâncias
+                do
+                {
+                    system("cls");
+                    printf("CONVERSÃO DE IMPEDÂNCIAS\nDigite:\n<a> Conversão polar -> retangular\n<b> Conversão retangular -> polar\n<r> Para retornar ao menu principal."); 
+                    user_sub_selection = toupper(getch());
+                }
+                while(user_sub_selection != 'R');
+                break; // Fim do submenu de conversão de impedâncias
+        case 'E':   // Divisor de Tensão
             do
             {
                 system("cls");
                 printf("DIVISOR DE TENSÃO\nDigite:\n<a> Para definir a tensão da fonte\n<b> Para definir o valor da impedância Z1\n<c> Para definir o valor da impedância Z2\n<r> Para retornar ao menu principal\n");
-                user_sub_selection = getch();
+                user_sub_selection = toupper(getch());
 
             }
-            while(user_sub_selection != 'r');
+            while(user_sub_selection != 'R');
             break;  // Fim do divisor de tensão
 
         default:
-            if(user_selection != 's')
+            if(user_selection != 'S')
             {
                 printf("Opção inválida, tente novamente.\n");
                 system("sleep 1");  // Aguarda 1 segundo antes de sair
